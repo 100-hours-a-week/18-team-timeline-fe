70f0fdf3dfd5354ff7c745dd4d7fe27e98a653db
chulsu0012 <chulsu0012@gmail.com>
2025-05-07 23:57:22 +0900

    feat: 모달 가로 길이 수정 및 입력창이 있는 모달 컴포넌트 구현 (#6)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -32,6 +32,7 @@ export default {
         modalButtonTextColor: '#FFFFFF',
         modalButtonConfirmColor: '#4F4F4F',
         modalButtonConfirmHoverColor: '#6F6F6F',
+        modalButtonDisactiveColor: '#D9D9D9',
         modalButtonCancelColor: '#9A9A9A',
         modalButtonCancelHoverColor: '#AAAAAA',
 
