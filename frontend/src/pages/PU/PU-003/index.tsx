4c3fe7cee3f2f2d63bc7234bb1aea07a1f0774eb
chulsu0012 <chulsu0012@gmail.com>
2025-05-07 23:59:23 +0900

    feat: 회원정보 수정 페이지 구현 (#6)

--- frontend/src/pages/PU/PU-003/index.tsx
+++ frontend/src/pages/PU/PU-003/index.tsx
@@ -1,6 +1,22 @@
+import { Wrapper } from "../components/layout/Wrapper";
+import { KaKaoButton } from "../components/ui/Button";
+import { UserInfoForm } from "./UserInfoForm";
+
 export default function UserInfoPage() {
+  const lineClass = 'border-t border-lineColor my-5';
+
   return (
-    <div>
+    <div className="wrap bg-puBgColor">
+      <Wrapper
+        title="회원정보 수정"
+        className="px-8"
+      >
+        <UserInfoForm /> 
+        <div className={lineClass} />
+        <KaKaoButton
+          accessToken={localStorage.getItem('accessToken') ?? ''}
+        />
+      </Wrapper>
     </div>
   )
 }
