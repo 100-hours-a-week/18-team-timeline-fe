6916b11ed74d95ff0aa0b2451c9d98c5f99de4ec
Haein <haeinjo@postech.ac.kr>
2025-05-07 17:58:22 +0900

    feat: banner in PN-001 added & library 'swiper' used

--- frontend/src/pages/PN-001/index.tsx
+++ frontend/src/pages/PN-001/index.tsx
@@ -1,6 +1,16 @@
+import React from 'react';
+import Banner from './Banner';
+import Category from './Category';
+import NewsList from './NewsList';
+
 export default function MainPage() {
+  const [selectedCategory, setSelectedCategory] = React.useState('all');
+
   return (
-    <div>
+    <div className="container mx-auto px-4 py-4">
+      <Banner />
+      <Category onCategoryChange={setSelectedCategory} />
+      <NewsList category={selectedCategory} />
     </div>
-  )
+  );
 }
