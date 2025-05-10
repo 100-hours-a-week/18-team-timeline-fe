2fca0013ee4c096c55139424f81ef2ebaf089bc2
chulsu0012 <chulsu0012@gmail.com>
2025-05-10 21:56:14 +0900

    fix: 카카오 소셜 로그인 버튼 제거

--- frontend/src/pages/PU/PU-003/index.tsx
+++ frontend/src/pages/PU/PU-003/index.tsx
@@ -1,10 +1,7 @@
 import { Wrapper } from "../components/layout/Wrapper";
-import { KaKaoButton } from "../components/ui/Button";
 import { UserInfoForm } from "./UserInfoForm";
 
 export default function UserInfoPage() {
-  const lineClass = 'border-t border-lineColor my-5';
-
   return (
     <div className="wrap bg-puBgColor">
       <Wrapper
@@ -12,10 +9,6 @@ export default function UserInfoPage() {
         className="px-8"
       >
         <UserInfoForm /> 
-        <div className={lineClass} />
-        <KaKaoButton
-          accessToken={localStorage.getItem('accessToken') ?? ''}
-        />
       </Wrapper>
     </div>
   )
