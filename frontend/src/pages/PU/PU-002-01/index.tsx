1a8bb2b22275463c1e7fcc7d48b851be7f2001fd
chulsu0012 <chulsu0012@gmail.com>
2025-06-11 14:20:04 +0900

    style: 비밀번호 찾기 페이지에 Prettier 포맷 적용 (#101)

--- frontend/src/pages/PU/PU-002-01/index.tsx
+++ frontend/src/pages/PU/PU-002-01/index.tsx
@@ -1,15 +1,12 @@
-import { Wrapper } from "../components/layout/Wrapper";
-import { FindPasswordForm } from "./FindPasswordForm";
+import { Wrapper } from '../components/layout/Wrapper'
+import { FindPasswordForm } from './FindPasswordForm'
 
 export default function FindPasswordPage() {
   return (
     <div className="wrap bg-puBg">
-      <Wrapper
-      title="비밀번호 찾기"
-      className="px-8"
-      >
-        <FindPasswordForm/>
+      <Wrapper title="비밀번호 찾기" className="px-8">
+        <FindPasswordForm />
       </Wrapper>
-  </div>
+    </div>
   )
 }
