876b14ca561fdee9edf26caacf3c8c0a384ffc34
chulsu0012 <chulsu0012@gmail.com>
2025-06-11 09:58:57 +0900

    refactor: 회원 페이지 공통 컴포넌트 분리

---
+++ frontend/src/pages/PU/utils/validateUserInfo.ts
@@ -0,0 +1,26 @@
+interface ValidationResult {
+  isValid: boolean
+  errors: {
+    name?: string
+  };
+}
+
+export function validateUserInfo(
+  name: string
+): ValidationResult {
+  const errors: ValidationResult["errors"] = {}
+  const nameRegex = /^[가-힣a-zA-Z0-9]+$/;
+  
+  if (!name.trim()) {
+    errors.name = "닉네임을 입력하세요.";
+  } else if (name.length > 10 || name.length < 2) {
+    errors.name = "2자 이상 10자 이내로 입력해 주세요.";
+  } else if (!nameRegex.test(name)) {
+    errors.name = "띄어쓰기 및 특수문자를 사용할 수 없습니다.";
+  }
+
+  return {
+    isValid: Object.keys(errors).length === 0,
+    errors,
+  };
+}
