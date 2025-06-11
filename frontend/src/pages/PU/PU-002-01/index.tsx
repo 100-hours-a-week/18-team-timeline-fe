f5689b4b2d323439085528b2e0aea7f1647c417f
chulsu0012 <chulsu0012@gmail.com>
2025-06-11 10:57:48 +0900

    feat: 비밀번호 찾기 페이지 UI 레이아웃 및 이메일 인증 API 연동 구현 (#95)

--- frontend/src/pages/PU/PU-002-01/index.tsx
+++ frontend/src/pages/PU/PU-002-01/index.tsx
@@ -1,6 +1,15 @@
+import { Wrapper } from "../components/layout/Wrapper";
+import { FindPasswordForm } from "./FindPasswordForm";
+
 export default function FindPasswordPage() {
   return (
-    <div>
-    </div>
+    <div className="wrap bg-puBg">
+      <Wrapper
+      title="비밀번호 찾기"
+      className="px-8"
+      >
+        <FindPasswordForm/>
+      </Wrapper>
+  </div>
   )
 }
