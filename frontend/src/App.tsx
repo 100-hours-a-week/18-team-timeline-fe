0cb7e968375388b99a867f687031a05614ee0fd7
chulsu0012 <chulsu0012@gmail.com>
2025-06-11 13:40:48 +0900

    feat: 회원정보 수정 페이지 라우트 추가 (#99)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import usePageStore from './stores/pageStore';
-import { ROUTES } from '@/constants/url';
-import Main from '@/pages/PN/PN-001';
-import NewsDetail from '@/pages/PN/PN-002';
-import SignUp from '@/pages/PU/PU-001';
-import Login from '@/pages/PU/PU-002';
-import UserInfo from '@/pages/PU/PU-003';
-import { Container } from './components/layout/Container';
-import { Header } from './components/layout/Header';
-import { Sidebar } from './components/layout/Sidebar';
+import React from 'react'
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './App.css'
+import usePageStore from './stores/pageStore'
+import { Container } from './components/layout/Container'
+import { Header } from './components/layout/Header'
+import { Sidebar } from './components/layout/Sidebar'
+import { ROUTES } from './constants/url'
+import UserInfo from './pages/PU/PU-003'
 
 const App: React.FC = () => {
-  const currentPage = usePageStore((state) => state.currentPage);
+  const currentPage = usePageStore((state) => state.currentPage)
 
   return (
     <Container>
@@ -22,15 +18,11 @@ const App: React.FC = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Navigate to={currentPage} />} />
-          <Route path={ROUTES.MAIN} element={<Main />} />
-          <Route path={ROUTES.NEWS_DETAIL} element={<NewsDetail />} />
-          <Route path={ROUTES.SIGNUP} element={<SignUp />} />
-          <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.USER_INFO} element={<UserInfo />} />
         </Routes>
       </Router>
     </Container>
-  );
-};
+  )
+}
 
-export default App;
+export default App
