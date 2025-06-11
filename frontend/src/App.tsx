7c455ba5b1fe3cd85fa6e5d96bf04f0f3537d12d
chulsu0012 <chulsu0012@gmail.com>
2025-06-11 13:54:52 +0900

    feat: 검색 결과 페이지 라우트 추가 (#99)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,15 @@
-import React from 'react';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import usePageStore from './stores/pageStore';
-import { ROUTES } from '@/constants/url';
-import Main from '@/pages/PN/PN-001';
-import SearchResults from '@/pages/PN/PN-002';
-import NewsDetail from '@/pages/PN/PN-003';
-import SignUp from '@/pages/PU/PU-001';
-import Login from '@/pages/PU/PU-002';
-import UserInfo from '@/pages/PU/PU-003';
-import { Container } from './components/layout/Container';
-import { Header } from './components/layout/Header';
-import { Sidebar } from './components/layout/Sidebar';
-import { KakaoCallback } from './pages/PU/auth/KakaoCallback';
+import React from 'react'
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './App.css'
+import usePageStore from './stores/pageStore'
+import { Container } from './components/layout/Container'
+import { Header } from './components/layout/Header'
+import { Sidebar } from './components/layout/Sidebar'
+import { ROUTES } from './constants/url'
+import SearchResults from './pages/PN/PN-002'
 
 const App: React.FC = () => {
-  const currentPage = usePageStore((state) => state.currentPage);
+  const currentPage = usePageStore((state) => state.currentPage)
 
   return (
     <Container>
@@ -24,17 +18,11 @@ const App: React.FC = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Navigate to={currentPage} />} />
-          <Route path={ROUTES.MAIN} element={<Main />} />
-          <Route path={ROUTES.SEARCH_RESULTS} element={<SearchResults searchQuery={''} />} />
-          <Route path={ROUTES.NEWS_DETAIL} element={<NewsDetail />} />
-          <Route path={ROUTES.KAKAO_CALLBACK} element={<KakaoCallback />} />
-          <Route path={ROUTES.SIGNUP} element={<SignUp />} />
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.USER_INFO} element={<UserInfo />} />
+          <Route path={ROUTES.SEARCH_RESULTS} element={<SearchResults searchQuery={''}/>} />
         </Routes>
       </Router>
     </Container>
-  );
-};
+  )
+}
 
-export default App;
+export default App
