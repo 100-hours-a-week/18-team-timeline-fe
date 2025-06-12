8a4fa8ed8b032805e269f452a5328a4c2220ab12
chulsu0012 <chulsu0012@gmail.com>
2025-06-13 08:24:45 +0900

    refactor: 비밀번호 입력 컴포넌트 분리 (#96)

--- frontend/src/pages/PU/PU-002-02/ResetPasswordForm.tsx
+++ frontend/src/pages/PU/PU-002-02/ResetPasswordForm.tsx
@@ -1,4 +1,3 @@
-import { Input } from '@/components/form/Input'
 import { useEffect, useState, type DetailedHTMLProps, type HTMLAttributes } from 'react'
 import clsx from 'clsx'
 import { Button } from '../components/ui/Button'
@@ -6,6 +5,10 @@ import { StaticField } from '../components/ui/StaticField'
 import { validateResetPassword } from '../utils/validateResetPassword'
 import { useResetPasswordLogic } from './useResetPasswordLogic'
 import { Toast } from '@/components/ui/Toast'
+import { PasswordInput } from '../components/form/PasswordInput'
+import { PasswordCheckInput } from '../components/form/PasswordCheckInput'
+import { ResetPasswordMessage } from '@/constants/PU/resetPasswordMessage'
+import { useAuthStore } from '@/stores/useAuthStore'
 
 type ReactDivProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 type ResetPasswordFormProps = ReactDivProps & {}
@@ -13,7 +16,6 @@ type ResetPasswordFormProps = ReactDivProps & {}
 export const ResetPasswordForm = ({}: ResetPasswordFormProps) => {
   const [toastMessage, setToastMessage] = useState('')
   const {
-    resetActionText,
     email,
     password,
     setPassword,
@@ -33,35 +35,26 @@ export const ResetPasswordForm = ({}: ResetPasswordFormProps) => {
   }, [password, passwordCheck])
 
   const formClass = clsx('w-full flex flex-col justify-center', 'space-y-3')
-
   const buttonClass = clsx('flex flex-col pt-8 space-y-1')
 
   return (
     <>
       <form className={formClass} onSubmit={handleSubmit}>
         <StaticField label="이메일" content={email} />
-        <Input
-          id="password"
-          labelName="비밀번호"
-          required={true}
-          type="password"
-          placeholder="비밀번호를 입력하세요."
+        <PasswordInput
+          isLabel={true}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           helperText={errors.password}
         />
-        <Input
-          id="password-check"
-          labelName="비밀번호 확인"
-          required={true}
-          type="password"
-          placeholder="비밀번호를 한 번 더 입력하세요."
+        <PasswordCheckInput
+          isLabel={true}
           value={passwordCheck}
           onChange={(e) => setPasswordCheck(e.target.value)}
           helperText={errors.passwordCheck}
         />
         <div className={buttonClass}>
-          <Button text={`비밀번호 ${resetActionText}하기`} isActive={isButtonActive} />
+          <Button text={ResetPasswordMessage.BTN_NAME(useAuthStore((state) => state.isLoggedIn))} isActive={isButtonActive} />
         </div>
       </form>
 
