d636204397004e68703f7e567df86cd46dc45f70
chulsu0012 <chulsu0012@gmail.com>
2025-06-12 11:05:17 +0900

    feat: 로그인 페이지 메시지 상수 모듈 추가 및 적용 (#109)

--- frontend/src/pages/PU/PU-002/LoginForm.tsx
+++ frontend/src/pages/PU/PU-002/LoginForm.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, type DetailedHTMLProps, type HTMLAttributes } from 'react'
 import clsx from 'clsx'
-import { Input } from '@/components/form/Input'
 import { Button } from '../components/ui/Button'
 import { Text } from '@/components/ui/Text'
 import { Link } from 'react-router-dom'
@@ -8,6 +7,11 @@ import { ROUTES, ENDPOINTS } from '@/constants/url'
 import { validateLogin } from '../utils/validateLogin'
 import { useNavigate } from 'react-router-dom'
 import { useRequestStore } from '@/stores/requestStore'
+import { EmailInput } from '../components/form/EmailInput'
+import { NavigationLink } from '@/constants/navigationLink'
+import { Toast } from '@/components/ui/Toast'
+import { LoginMessage } from '@/constants/PU/loginMessage'
+import { PasswordInput } from '../components/form/PasswordInput'
 
 type ReactDivProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 type LoginFormProps = ReactDivProps & {}
@@ -17,6 +21,7 @@ export const LoginForm = ({}: LoginFormProps) => {
   const [password, setPassword] = useState('')
   const [errors, setErrors] = useState<{ email?: string; password?: string }>({})
   const [isButtonActive, setIsButtonActive] = useState(false)
+  const [toastMessage, setToastMessage] = useState('')
 
   useEffect(() => {
     const result = validateLogin(email, password)
@@ -42,49 +47,43 @@ export const LoginForm = ({}: LoginFormProps) => {
         navigate(ROUTES.MAIN)
       }
     } catch (error) {
-      console.error('로그인 실패', error)
-      alert('로그인 중 오류가 발생했습니다.')
+      console.error('로그인 실패:', error)
+      setToastMessage(LoginMessage.TOAST_FAIL)
     }
   }
 
   const formClass = clsx('w-full flex flex-col justify-center', 'space-y-3')
-
   const buttonClass = clsx('flex flex-col pt-8 space-y-1')
-
   const navigationClass = clsx('w-full justify-center flex flex-row space-x-1', 'text-sm text-navText')
-
   const linkClass = clsx('hover:text-navTextHover')
 
   return (
-    <form className={formClass} onSubmit={handleSubmit}>
-      <Input
-        id="email"
-        type="text"
-        placeholder="이메일을 입력하세요."
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        helperText={errors.email}
-      />
-      <Input
-        id="password"
-        type="password"
-        placeholder="비밀번호를 입력하세요."
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        helperText={errors.password}
-      />
-      <div className={buttonClass}>
-        <Button text="로그인하기" isActive={isButtonActive} />
-        <div className={navigationClass}>
-          <Link to={ROUTES.SIGNUP} className={linkClass}>
-            <Text>회원가입</Text>
-          </Link>
-          <Text>|</Text>
-          <Link to={''} className={linkClass}>
-            <Text>비밀번호 찾기</Text>
-          </Link>
+    <>
+      <form className={formClass} onSubmit={handleSubmit}>
+        <EmailInput
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          helperText={errors.email}
+        />
+        <PasswordInput
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          helperText={errors.password}
+        />
+        <div className={buttonClass}>
+          <Button text={LoginMessage.BTN_NAME} isActive={isButtonActive} />
+          <div className={navigationClass}>
+            <Link to={ROUTES.SIGNUP} className={linkClass}>
+              <Text>{NavigationLink.SIGNUP}</Text>
+            </Link>
+            <Text>|</Text>
+            <Link to={''} className={linkClass}>
+              <Text>{NavigationLink.FIND_PASSWORD}</Text>
+            </Link>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+      {toastMessage && <Toast message={toastMessage} />}
+    </>
   )
 }
