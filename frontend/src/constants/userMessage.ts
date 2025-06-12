9d2ee9cc8d87cc5e8327b5a0aafdc648145214de
chulsu0012 <chulsu0012@gmail.com>
2025-06-12 10:08:09 +0900

    feat: userMessage 상수 클래스에 비밀번호 찾기 페이지의 메시지 추가 (#95)

---
+++ frontend/src/constants/userMessage.ts
@@ -0,0 +1,6 @@
+export const userMessage = {
+  FIND_PASSWORD_TITLE: '비밀번호 찾기',
+  FIND_PASSWORD_CONTENT: '이메일로 비밀번호 재설정 링크를 보내드립니다.',
+  FIND_PASSWORD_TOAST_SUCCESS: '비밀번호 재설정 링크를 이메일로 보내드렸습니다.',
+  FIND_PASSWORD_TOAST_FAIL: '잠시 후 다시 시도해 주세요.',
+}
