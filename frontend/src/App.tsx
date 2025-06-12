e0ec0704a8c860194babe11f88e639f819ef8179
chulsu0012 <chulsu0012@gmail.com>
2025-06-12 13:35:40 +0900

    feat: 카카오 로그인 콜백 처리 라우트 추가 (#111)
    - /auth/kakao/callback 경로에 대한 라우트 등록
    - 토큰 처리 및 로그인 상태 반영을 위한 KakaoCallback 컴포넌트 연동

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,14 @@ import { Container } from './components/layout/Container'
 import { Header } from './components/layout/Header'
 import { Sidebar } from './components/layout/Sidebar'
 import { ROUTES } from './constants/url'
+import { LogoutRoute } from './routes/LogoutRoute'
+import { LoginRoute } from './routes/LoginRoute'
 import MainPage from './pages/PN/PN-001'
 import NewsDetail from './pages/PN/PN-003'
 import SignUp from './pages/PU/PU-001'
-import { LogoutRoute } from './routes/LogoutRoute'
 import Login from './pages/PU/PU-002'
+import { KakaoCallback } from './pages/PU/auth/KakaoCallback'
 import UserInfo from './pages/PU/PU-003'
-import { LoginRoute } from './routes/LoginRoute'
 import FindPassword from './pages/PU/PU-002-01'
 import SearchResults from './pages/PN/PN-002'
 
@@ -46,6 +47,14 @@ const App: React.FC = () => {
             element={
               <LogoutRoute>
                 <Login />
+              </LogoutRoute>
+            }
+          />
+          <Route
+            path={ROUTES.KAKAO_CALLBACK}
+            element={
+              <LogoutRoute>
+                <KakaoCallback />
               </LogoutRoute>
             }
           />
