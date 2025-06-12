3e19143f074f1e141da90e6cd8c364c976a6144b
chulsu0012 <chulsu0012@gmail.com>
2025-06-12 10:14:59 +0900

    feat: 비밀번호 찾기 페이지 Wrapper에 페이지 안내 문구 추가 (#95)
    - 비밀번호 찾기 안내를 위한 설명 텍스트(content) prop 전달

--- frontend/src/pages/PU/PU-002-01/index.tsx
+++ frontend/src/pages/PU/PU-002-01/index.tsx
@@ -1,10 +1,11 @@
+import { userMessage } from '@/constants/userMessage'
 import { Wrapper } from '../components/layout/Wrapper'
 import { FindPasswordForm } from './FindPasswordForm'
 
 export default function FindPasswordPage() {
   return (
     <div className="wrap bg-puBg">
-      <Wrapper title="비밀번호 찾기" className="px-8">
+      <Wrapper title={userMessage.FIND_PASSWORD_TITLE} content={userMessage.FIND_PASSWORD_CONTENT} className="px-8">
         <FindPasswordForm />
       </Wrapper>
     </div>
