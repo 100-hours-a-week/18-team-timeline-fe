1afd972d0cbbcdee271ade8a06bd685f768e68c6
chulsu0012 <chulsu0012@gmail.com>
2025-06-13 21:03:32 +0900

    refactor: AppContent 경로를 components/layout으로 변경 (#93)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 import { Container } from './components/layout/Container'
-import { AppContent } from './AppContent'
+import { AppContent } from './components/layout/AppContent'
 
 const App: React.FC = () => {
   return (
