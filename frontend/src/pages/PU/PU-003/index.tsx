bb102645cc7454669fd70e6b37d58ae017b5aa1b
chulsu0012 <chulsu0012@gmail.com>
2025-06-16 10:18:34 +0900

    refactor: 공통 Wrapper 컴포넌트 파일 경로 리팩터링 (#97)

--- frontend/src/pages/PU/PU-003/index.tsx
+++ frontend/src/pages/PU/PU-003/index.tsx
@@ -1,5 +1,5 @@
 import { UserInfoMessage } from '@/constants/PU/userInfoMessage'
-import { Wrapper } from '../components/layout/Wrapper'
+import { Wrapper } from '../../../components/layout/Wrapper'
 import { UserInfoForm } from './UserInfoForm'
 
 export default function UserInfoPage() {
