40ba79c637a008006ac57ad9c6a971454088db2f
chulsu0012 <chulsu0012@gmail.com>
2025-06-30 14:44:10 +0900

    feat: hasVoted에 따른 투표 폼 활성화 분기 처리 및 제출한 선택지를 setSelectOps()로 초기화 (#220)

--- frontend/src/constants/PV/pollMessage.ts
+++ frontend/src/constants/PV/pollMessage.ts
@@ -2,6 +2,7 @@ export const PollMessage = {
   TITLE: (title: string) => `${title}`,
   CONTENT: (endAt: string) => `투표가 ${endAt}에 종료됩니다.`,
   BTN_NAME: '투표하기',
+  BTN_VOTED_NAME: '투표 완료',
   METATEXT: '투표는 한 번만 가능하며, 이후에는 변경이 불가합니다.',
   TOAST_SUCCESS: '투표를 제출하였습니다.',
   TOAST_FAIL: '투표를 제출하는 중에 오류가 발생하였습니다.'
