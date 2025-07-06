dee41643c624b440d639a94a81f6c7edb60d9986
[chulsu0012] <[chulsu0012@gmail.com]>
2025-07-06 21:53:46 +0900

    refactor: 배경 색상 패턴 및 배경 아이템 PNG 애니메이션 효과 추가 (#224)
    - 배경 색상 변경 및 도트 디자인 적용
    - 배경 아이템 PNG 애니메이션 효과 적용
    - 컨테이너에 그림자 CSS 추가
    - Wrapper에 그림자 CSS 추가

--- frontend/src/components/layout/Container.tsx
+++ frontend/src/components/layout/Container.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import type { DetailedHTMLProps, HTMLAttributes } from 'react'
 import clsx from 'clsx'
+import { BgItem1, BgItem2, BgItem3 } from '@/assets'
 
 type ReactDivProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 
@@ -8,7 +9,7 @@ type ContainerProps = ReactDivProps & {
   children: React.ReactNode
 }
 
-export const Container = ({ className: _className, children }: ContainerProps) => {
+export const Container = ({ children }: ContainerProps) => {
   const [isSmall, setIsSmall] = useState(false)
 
   useEffect(() => {
@@ -21,16 +22,39 @@ export const Container = ({ className: _className, children }: ContainerProps) =
     return () => window.removeEventListener('resize', handleResize)
   }, [])
 
-  const containerClass = 'flex items-center justify-center min-h-screen bg-mainBgColor'
+  const containerClass = clsx(
+    'flex items-center justify-center min-h-screen bg-mainBg',
+    'bg-[radial-gradient(theme(colors.mainPoint)_1.5px,transparent_1px)]',
+    'bg-[size:60px_60px]',
+    'relative overflow-hidden',
+  )
 
   const wrapperClass = clsx(
     isSmall ? 'w-full' : 'w-full max-w-[390px]',
-    'h-screen flex flex-col mx-auto bg-myWhite relative overflow-hidden',
+    'h-screen flex flex-col mx-auto bg-myWhite relative overflow-hidden shadow-lg z-30',
   )
+
+  const bgGradationClass = 'absolute bottom-0 left-0 z-20 w-full h-1/3 bg-gradient-to-t from-white to-transparent'
+
+  const bgItemClass = 'absolute top-0 object-contain h-3/4 z-10'
+  const bgItemBottomContainerClass = 'absolute bottom-[-20%] right-[-18%] w-2/3 overflow-visible z-10'
+  const bgItemBottomClass = 'object-contain h-full animate-rise-up'
 
   return (
     <div className={containerClass}>
       <div className={wrapperClass}>{children}</div>
+
+      <div className={bgGradationClass} />
+      <div className={bgItemBottomContainerClass} style={{ height: '60%' }}>
+        <img src={BgItem3} className={bgItemBottomClass} style={{ display: 'block' }} alt="background bottom" />
+      </div>
+
+      <img src={BgItem1} className={clsx(bgItemClass, 'left-0 animate-slide-in-left')} alt="background left" />
+      <img
+        src={BgItem2}
+        className={clsx(bgItemClass, 'right-0 scale-x-[-1] animate-slide-in-right')}
+        alt="background right"
+      />
     </div>
   )
 }
