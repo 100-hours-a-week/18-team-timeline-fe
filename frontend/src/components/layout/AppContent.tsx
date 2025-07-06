4fc4c03c0a734c4fd3247471b288c1d9af624294
[chulsu0012] <[chulsu0012@gmail.com]>
2025-07-06 22:35:13 +0900

    refactor: 메뉴 사이드바 및 알림창 표시 시 배경에 검정 오버레이 추가 (#227)

--- frontend/src/components/layout/AppContent.tsx
+++ frontend/src/components/layout/AppContent.tsx
@@ -9,27 +9,33 @@ import { LogoutRoute } from '../../routes/LogoutRoute'
 import { LoginRoute } from '../../routes/LoginRoute'
 import MainPage from '../../pages/PN/PN-001'
 import NewsDetail from '../../pages/PN/PN-003'
-import SignUp from '../../pages/PU/PU-001'
 import Login from '../../pages/PU/PU-002'
 import { KakaoCallback } from '../../pages/PU/auth/KakaoCallback'
 import UserInfo from '../../pages/PU/PU-003'
-import FindPassword from '../../pages/PU/PU-002-01'
 import SearchResults from '../../pages/PN/PN-002'
 import { useAuthStore } from '../../stores/useAuthStore'
-import ResetPassword from '../../pages/PU/PU-002-02'
-import { ResetPasswordRoute } from '../../routes/ResetPasswordRoute'
 import { SearchBar } from './SearchBar/SearchBar'
 import { useSearchBarStore } from '../../stores/useSearchBarStore'
 import { useSearchStore } from '../../stores/useSearchStore'
 import { SidebarAlarm } from './SidebarAlarm/SidebarAlarm'
 import PollPage from '@/pages/PV/PV-001'
 import BookmarkPage from '@/pages/PN/PN-004'
+import { useSidebarMenuStore } from '@/stores/useSidebarMenuStore'
+import { useSidebarAlarmStore } from '@/stores/useSidebarAlarmStore'
 
 export const AppContent = () => {
   const location = useLocation()
   const currentPage = usePageStore((state) => state.currentPage)
   const isSearchOpen = useSearchBarStore((state) => state.isOpen)
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn)
+
+  const isMenuOpen = useSidebarMenuStore((state) => state.isOpen)
+  const closeMenu = useSidebarMenuStore((state) => state.close)
+
+  const isAlarmOpen = useSidebarAlarmStore((state) => state.isOpen)
+  const closeAlarm = useSidebarAlarmStore((state) => state.close)
+
+  const overlayClass = 'absolute top-0 left-0 z-40 w-full h-full bg-black opacity-50'
 
   useEffect(() => {
     useAuthStore.getState().checkAuth()
@@ -47,45 +53,33 @@ export const AppContent = () => {
     }
   }, [location.pathname])
 
+  const handleOverlayClick = () => {
+    if (isMenuOpen) closeMenu()
+    if (isAlarmOpen) closeAlarm()
+  }
+
   return (
     <Container>
+      {(isMenuOpen || isAlarmOpen) && <div className={overlayClass} onClick={handleOverlayClick} />}
+
       <SidebarMenu />
       {isLoggedIn && <SidebarAlarm />}
       {isSearchOpen ? <SearchBar /> : <Header />}
 
       <Routes>
         <Route path="/" element={<Navigate to={currentPage} />} />
-
-        {/* 로그인 여부와 상관없이 접근 가능 */}
         <Route path={ROUTES.MAIN} element={<MainPage />} />
         <Route path={ROUTES.SEARCH_RESULTS} element={<SearchResults />} />
         <Route path={ROUTES.NEWS_DETAIL} element={<NewsDetail />} />
 
-        {/* 로그아웃 상태에서만 접근 가능 */}
-        {/* <Route element={<LogoutRoute />}>
-          <Route path={ROUTES.SIGNUP} element={<SignUp />} />
-        </Route> */}
         <Route element={<LogoutRoute />}>
           <Route path={ROUTES.LOGIN} element={<Login />} />
-        </Route>
-        <Route element={<LogoutRoute />}>
           <Route path={ROUTES.KAKAO_CALLBACK} element={<KakaoCallback />} />
         </Route>
-        {/* <Route element={<LogoutRoute />}>
-          <Route path={ROUTES.FIND_PASSWORD} element={<FindPassword />} />
-        </Route>
-        <Route element={<ResetPasswordRoute />}>
-          <Route path={ROUTES.RESET_PASSWORD} element={<ResetPassword />} />
-        </Route> */}
 
-        {/* 로그인 상태에서만 접근 가능 */}
         <Route element={<LoginRoute />}>
           <Route path={ROUTES.USER_INFO} element={<UserInfo />} />
-        </Route>
-        <Route element={<LoginRoute />}>
           <Route path={ROUTES.POLL} element={<PollPage />} />
-        </Route>
-        <Route element={<LoginRoute />}>
           <Route path={ROUTES.BOOKMARK} element={<BookmarkPage />} />
         </Route>
       </Routes>
