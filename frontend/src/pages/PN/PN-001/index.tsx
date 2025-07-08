e1b2b6e3a9516abd2c19937c4e6d8abe3dab8fcf
phUR99 <skerrigen12@gmail.com>
2025-07-08 13:01:00 +0900

    feat: 메인 페이지의 레이아웃 개선 및 컨테이너 클래스 수정

--- frontend/src/pages/PN/PN-001/index.tsx
+++ frontend/src/pages/PN/PN-001/index.tsx
@@ -12,10 +12,10 @@ export default function MainPage({ initialCategory = 'all' }: MainPageProps) {
   const [selectedCategory, setSelectedCategory] = useState(initialCategory)
   const { newsByCategory, fetchNews } = useFetchNormal()
 
-  const carouselContainerClass = 'container mx-auto px-4 pt-6 pb-4 flex-shrink-0'
+  const carouselContainerClass = 'flex flex-col items-center w-full mx-auto px-4 pt-6 pb-4 flex-shrink-0'
 
   return (
-    <div className="wrap">
+    <div className="flex flex-col w-full">
       <div className={carouselContainerClass}>
         <Carousel />
       </div>
